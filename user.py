60c0c4df210c46dbf8082156460d6c6ee9f530e8
jbqh <jbqh@sina.com>
2018-09-05 22:33:16 +0800

    Create user.py

---
+++ user.py
@@ -0,0 +1,17 @@
+# manage users
+# add,delete,alter
+
+import MySQLdb
+
+def add_user(username);
+	pass
+	
+def del_user(username):
+	pass
+	
+def alt_user(username,**args):
+	pass
+
+def get_mysql_connection(user,host,port=3306,dbname):
+	pass
+	
