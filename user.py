3a042330a0803508453cfbfe30ad5a5297cf56ea
jbqh <jbqh@sina.com>
2018-09-07 21:45:44 +0800

    Update user.py

--- user.py
+++ user.py
@@ -4,7 +4,25 @@
 import MySQLdb
 
 def add_user(username);
-	pass
+	cur = get_mysql_connection(1)
+	sql = 'select id from users where username=%s' % username
+	cur.execute(sql)
+	rst = cur.fetchall()
+	if rst:
+		print 'User %s is already exist, Exit!'
+		exit(10)
+	sj = time.strftime('%F %T')
+	display_name = display_name
+	allow_login = sj
+	last_login = sj
+	
+	sql = 'insert into users(username,display_name,allow_login,last_login) values (%s %s %s %s)' % (username,display_name,allow_login,last_login)
+	try:
+		cur.execute(sql)
+		return 0
+	except Exception as e
+		print e
+		exit(20)
 	
 def del_user(username):
 	pass
@@ -12,6 +30,21 @@ def del_user(username):
 def alt_user(username,**args):
 	pass
 
-def get_mysql_connection(user,host,port=3306,dbname):
-	pass
+def get_mysql_connection(cursor_type=1):
+	host = '172.16.172.100'
+	username = 'openvpn'
+	password = 'vdin1234'
+	port = 3306
+	
+	try:
+		conn = MySQLdb.connect(host=host,user=username,passwd=password,db=dbname,port=port)
+		if cursor_type == 1:
+			cursor = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
+		else:
+			cursor = conn.cursor()
+		return cursor
+	
+	except Exception as e:
+		print e
+		exit(1)
 	
