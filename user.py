1b4659121b40a12454deeac9cc956d72cbe375e1
jbqh <jbqh@sina.com>
2018-09-07 22:23:31 +0800

    Update user.py

--- user.py
+++ user.py
@@ -4,35 +4,39 @@
 import MySQLdb
 
 def add_user(username,displayname);
-	cur = get_mysql_connection(1)
-	sql = 'select id from users where username=%s' % username
+	(cur,conn) = get_mysql_connection(1)
+	sql = 'select id from users where username="%s"' % username
 	cur.execute(sql)
 	rst = cur.fetchall()
 	if rst:
-		print 'User %s is already exist, Exit!'
+		print 'User [%s] is already exist, Exit!' % username
 		exit(10)
 	sj = time.strftime('%F %T')
 	display_name = display_name
 	allow_login = sj
 	last_login = sj
 	
-	sql = 'insert into users(username,display_name,allow_login,last_login) values (%s %s %s %s)' % (username,display_name,allow_login,last_login)
+	sql = 'insert into users(username,display_name,allow_login,last_login) values ("%s","%s","%s","%s")' % (username,display_name,allow_login,last_login)
 	try:
 		cur.execute(sql)
+		conn.commit()
 		return 0
 	except Exception as e
 		print e
+		conn.rollback()
 		exit(20)
 	conn.close()
 	
 def del_user(username):
-	cur = get_mysql_connection(1)
-	sql = 'delete from users where username=%s' % username
+	(cur,conn) = get_mysql_connection(1)
+	sql = 'delete from users where username="%s"' % username
 	try:
 		cur.execute(sql)
+		conn.commit()
 		return 0
 	except Exception as e:
 		print e
+		conn.rollback()
 		return(20)
 	conn.close()
 	
